1adbae9a6cbfee2b1ec69c2f03b0a72195118bc0
rahydyn <kanaokasatoru@gmail.com>
2022-05-11 14:16:44 +0900

    first commit

---
+++ js/myr.js
@@ -0,0 +1,131 @@
+function initialize() {
+  let input_total = document.getElementById("input_total").value;
+  let input_period = document.getElementById("input_period").value;
+  let input_permonth = document.getElementById("input_permonth").value;
+  let input_savings = document.getElementById("input_savings").value;
+}
+
+function flgDone() {
+    
+}
+
+function generate_year_range(start, end) {
+  let years = "";
+  for (let year = start; year <= end; year++) {
+    years += "<option value='" + year + "'>" + year + "</option>";
+  }
+  return years;
+}
+
+let today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+let selectYear = document.getElementById("year");
+let selectMonth = document.getElementById("month");
+
+let createYear = generate_year_range(2020, 2070);
+
+document.getElementById("year").innerHTML = createYear;
+
+let calendar = document.getElementById("calendar");
+let lang = calendar.getAttribute("data-lang");
+
+let months = [
+  "1月",
+  "2月",
+  "3月",
+  "4月",
+  "5月",
+  "6月",
+  "7月",
+  "8月",
+  "9月",
+  "10月",
+  "11月",
+  "12月",
+];
+let days = ["日", "月", "火", "水", "木", "金", "土"];
+
+let dayHeader = "<tr>";
+for (day in days) {
+  dayHeader += "<th data-days='" + days[day] + "'>" + days[day] + "</th>";
+}
+dayHeader += "</tr>";
+
+document.getElementById("thead-month").innerHTML = dayHeader;
+
+monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
+
+function next() {
+  currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
+  currentMonth = (currentMonth + 1) % 12;
+  showCalendar(currentMonth, currentYear);
+}
+
+function previous() {
+  currentYear = currentMonth === 0 ? currentYear - 1 : currentYear;
+  currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
+  showCalendar(currentMonth, currentYear);
+}
+
+function jump() {
+  currentYear = parseInt(selectYear.value);
+  currentMonth = parseInt(selectMonth.value);
+  showCalendar(currentMonth, currentYear);
+}
+
+function showCalendar(month, year) {
+  let firstDay = new Date(year, month).getDay();
+
+  tbl = document.getElementById("calendar-body");
+
+  tbl.innerHTML = "";
+
+  monthAndYear.innerHTML = year + "年 " + months[month];
+  selectYear.value = year;
+  selectMonth.value = month;
+
+  // creating all cells
+  let date = 1;
+  for (let i = 0; i < 6; i++) {
+    let row = document.createElement("tr");
+
+    for (let j = 0; j < 7; j++) {
+      if (i === 0 && j < firstDay) {
+        cell = document.createElement("td");
+        cellText = document.createTextNode("");
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      } else if (date > daysInMonth(month, year)) {
+        break;
+      } else {
+        cell = document.createElement("td");
+        cell.setAttribute("data-date", date);
+        cell.setAttribute("data-month", month + 1);
+        cell.setAttribute("data-year", year);
+        cell.setAttribute("data-month_name", months[month]);
+        cell.className = "date-picker";
+        cell.innerHTML = "<span>" + date + "</span>";
+        // TODO: 完了されている日は変化させる
+
+        if (
+          date === today.getDate() &&
+          year === today.getFullYear() &&
+          month === today.getMonth()
+        ) {
+          cell.className = "date-picker selected";
+          // TODO: 完了ボタン入れる
+        }
+        row.appendChild(cell);
+        date++;
+      }
+    }
+
+    tbl.appendChild(row);
+  }
+}
+
+function daysInMonth(iMonth, iYear) {
+  return 32 - new Date(iYear, iMonth, 32).getDate();
+}
